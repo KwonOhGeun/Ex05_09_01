9022f08672457bab940b67d023b20d616af83a11
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-04-06 18:18:09 +0900

    Initial commit

---
+++ src/main/java/Ex05_09_01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex05_09_01 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("1~4중에 선택하세요 : ");
+        a = s.nextInt();
+
+        if (a == 1) {
+            System.out.println("1을 선택했다.");
+        } else if (a == 2) {
+            System.out.println("2를 선택했다.");
+        } else if (a == 3) {
+            System.out.println("3을 선택했다.");
+        } else if (a == 4) {
+            System.out.println("4를 선택했다.");
+        } else if
+            ((((a != 1) && (a != 2) && (a != 3) && (a != 4)))){
+            System.out.println("이상한걸 선택했다.");
+        }
+    }
+}
